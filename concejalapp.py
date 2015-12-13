1210352e4a58309f77dfdb787bae022770317986
HJulio <h.julio@icloud.com>
2015-12-13 03:32:28 +0100

    Palabras extra

--- concejalapp.py
+++ concejalapp.py
@@ -10,50 +10,50 @@ app = Flask(__name__)
 app.config['ALLOWED_EXTENSIONS'] = set(['csv'])
 
 
-ETA_words = ['ETA', 'eta', 'matar', 'etarra', 'etarras', 'Cifuentes', 'judio', 'Israel',
-             'bomba', 'bombas', 'Aguirre', 'Podemos', 'Venezuela']
-
-
-def process(file):
-
-    ETA_dic = {}
-    reader = csv.reader(file)
-    for row in reader:
-        for word in row[5].split():
-            if word in ETA_words:
-                ETA_dic[row[0]] = row[5]
-                break
-    return ETA_dic
+def process(file_, words):
+	eta_dic = {}
+	reader = csv.reader(file_)
+	for row in reader:
+		for word in row[5].split():
+			if word in words:
+				eta_dic[row[0]] = row[5]
+				break
+	return eta_dic
 
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+	return '.' in filename and \
+	       filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+	return render_template('index.html')
 
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    file = request.files['file']
-    if file and allowed_file(file.filename):
-        d=process(file)
-        return render_template('tweets.html', ETA_dic=d)
+	eta_words = ['ETA', 'eta', 'matar', 'etarra', 'etarras', 'Cifuentes', 'judio', 'Israel', 'bomba', 'bombas', 'Aguirre', 'Podemos', 'Venezuela']
 
-    else:
-        return redirect(url_for('error'))
+	file_ = request.files['file']
+	for w in request.form['palabrasExtra'].split(','):
+		eta_words.append(w.strip(' '))
+
+	if file_ and allowed_file(file_.filename):
+		d = process(file_, eta_words)
+		return render_template('tweets.html', ETA_dic=d)
+
+	else:
+		return redirect(url_for('error'))
 
 
 # If error (wrong extension / empty)
 @app.route('/error')
 def error():
-    return render_template('error.html')
+	return render_template('error.html')
 
 
 if __name__ == '__main__':
-    app.run(
-        host="localhost"
-    )
+	app.run(
+			host="localhost"
+	)
